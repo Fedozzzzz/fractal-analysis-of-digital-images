6be9b64dc10317532f452d08620d3500645fe5e0
FedozZz <titov.bigf@gmail.com>
2021-03-22 12:23:31 +0300

    Fixed lab 4 - calculate Minkowski dimension with MFS method for grayscale image.

--- lab4/main.py
+++ lab4/main.py
@@ -6,41 +6,23 @@ def get_image_resolution(img):
     return img.shape[0], img.shape[1]
 
 
-def get_minkowski_dimension(input_img):
-    # convert input image to grayscale
-    img_gs = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-
-    # applying thresholding
-    img = cv2.threshold(img_gs, 127, 255, cv2.THRESH_BINARY)[1] / 255
-
-    # deltas array (δ-array)
-    deltas = [1, 2, 3]
-
+def get_A(img, deltas):
     w, h = get_image_resolution(img)
 
-    # cell size
-    n = 100
-
-    # crop image array by cell size
-    S = np.add.reduceat(
-        np.add.reduceat(img, np.arange(0, w, n), axis=0),
-        np.arange(0, h, n), axis=1)
-
-    S_w = S.shape[0]
-    S_h = S.shape[1]
-
-    u = np.zeros((len(deltas) + 1, S_w + 1, S_h + 1))
-    b = np.zeros((len(deltas) + 1, S_w + 1, S_h + 1))
-    Vol = np.zeros(len(deltas) + 1)
+    u = np.zeros((len(deltas) + 1, w + 1, h + 1))
+    b = np.zeros((len(deltas) + 1, w + 1, h + 1))
+    Vol = np.zeros((len(deltas) + 1))
+    A = np.zeros((len(deltas) + 1))
 
     # applying MFS method
-    for delta in deltas:
-        for i in range(0, S_w):
-            for j in range(0, S_h):
-                u[0][i][j] = S[i][j]
-                b[0][i][j] = S[i][j]
+    # for each cell calculate Vol
+    for i in range(0, w):
+        for j in range(0, h):
+            u[0][i][j] = img[i][j]
+            b[0][i][j] = img[i][j]
 
-                # calculate the top surface (u) and bottom surface (b)
+            # calculate the top surface (u) and bottom surface (b)
+            for delta in deltas:
                 u[delta][i][j] = max(u[delta - 1][i][j] + 1,
                                      max(u[delta - 1][i + 1][j + 1], u[delta - 1][i - 1][j + 1],
                                          u[delta - 1][i + 1][j - 1],
@@ -50,20 +32,53 @@ def get_minkowski_dimension(input_img):
                                          b[delta - 1][i + 1][j - 1],
                                          b[delta - 1][i - 1][j - 1]))
 
-        # the volume of a δ-parallel body is calculated as
+    # the volume of a δ-parallel body is calculated as
+    for delta in deltas:
         summa = 0
-        for i in range(0, S_w):
-            for j in range(0, S_h):
-                summa += u[delta][i][j] - b[delta][i][j]
-
+        for x in range(0, w):
+            for y in range(0, h):
+                summa += u[delta][x][y] - b[delta][x][y]
         Vol[delta] = summa
 
-    squares_A = []
     for delta in deltas:
-        squares_A.append(((Vol[delta] - Vol[delta - 1]) / 2))
+        A[delta] = ((Vol[delta] - Vol[delta - 1]) / 2)
+
+    return A
+
+
+def get_minkowski_dimension(image):
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # deltas array (δ-array)
+    deltas = [1, 2]
+
+    w, h = get_image_resolution(img)
+
+    cell_size = 50
+
+    c_w = int(w / cell_size)
+    c_h = int(h / cell_size)
+
+    A = np.zeros((c_w, c_h, len(deltas) + 1))
+
+    for x in range(0, c_w):
+        for y in range(0, c_h):
+            crop_img = img[y * cell_size:cell_size * (y + 1), x * cell_size:cell_size * (x + 1)]
+            res = get_A(crop_img, deltas)
+            for delta in deltas:
+                A[x][y][delta] = res[delta]
+
+    deltas_A = []
+    for delta in deltas:
+        s = 0
+        for x in range(0, c_w):
+            for y in range(0, c_h):
+                s += A[x][y][delta]
+        deltas_A.append(s)
+
 
     # applying least square method for calculating logarithms for each cell
-    res = np.polyfit(np.log(squares_A), np.log(deltas), 1)
+    res = np.polyfit(np.log(deltas_A), np.log(deltas), 1)
     return 2 - -res[0]
 
 
@@ -81,12 +96,12 @@ if __name__ == '__main__':
 
     """
      RESULT IS: 
-    ./images/1.jpg: 1.3833961452228705
-    ./images/2.jpg: 1.7061064915712267
-    ./images/3.jpg: 1.6024032195980311
-    ./images/4.jpg: 1.7895351151785681
-    ./images/image.jpg: 1.6464720914522937
-    ./images/1.png: 1.5721023387422974
-    ./images/m1.png: 0.992093955126383
-    ./images/russia.png: 1.8706248059568238
+    ./images/1.jpg: 1.625568806000594
+    ./images/2.jpg: 1.7845237036139086
+    ./images/3.jpg: 1.7646270934933632
+    ./images/4.jpg: 1.8405488686991418
+    ./images/image.jpg: 1.7326906497144254
+    ./images/1.png: 1.8060886139445047
+    ./images/m1.png: 1.5368203460200918
+    ./images/russia.png: 1.831861523276633
     """
